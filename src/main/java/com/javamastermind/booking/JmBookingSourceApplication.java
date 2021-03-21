a5ef23d2271cc117d62faf132dc8cfdeb81fc549
Lahiru Wijesekara <lahirusandaruwan@gmail.com>
2021-03-21 11:25:31 +0530

    Project initialization
    First commit

---
+++ src/main/java/com/javamastermind/booking/JmBookingSourceApplication.java
@@ -0,0 +1,13 @@
+package com.javamastermind.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmBookingSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmBookingSourceApplication.class, args);
+	}
+
+}
