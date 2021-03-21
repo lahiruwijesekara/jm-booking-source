4cc50749a8182a834bf9f36f89d5f5fab188ba39
Lahiru Wijesekara <lahirusandaruwan@gmail.com>
2021-03-21 19:23:55 +0530

    Event publishing
    1. Event publishing
    2. Kafka integration
    3. Booking object updates

--- src/main/java/com/javamastermind/booking/JmBookingSourceApplication.java
+++ src/main/java/com/javamastermind/booking/JmBookingSourceApplication.java
@@ -1,19 +1,41 @@
 package com.javamastermind.booking;
 
+import java.util.UUID;
+import java.util.function.Supplier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.javamastermind.booking.domain.Booking;
 
 /**
  * @author lahiru_w
- *
  */
 @SpringBootApplication
 public class JmBookingSourceApplication
 {
+    private static final Logger log = LoggerFactory.getLogger(JmBookingSourceApplication.class);
 
     public static void main(String[] args)
     {
         SpringApplication.run(JmBookingSourceApplication.class, args);
+        log.info("Booking source application has started to run");
     }
 
+    @Bean
+    public Supplier<Booking> bookingOrder()
+    {
+
+        Supplier<Booking> bookingSupplier = () -> {
+            Booking booking = new Booking(UUID.randomUUID().toString(), "100", "1200.00");
+            log.info("{} {} for ${} for {}", booking.getStatus(), booking.getBookingTranUUId(),
+                booking.getBookedPrice(), booking.getUserId());
+            return booking;
+        };
+
+        return bookingSupplier;
+    }
 }
